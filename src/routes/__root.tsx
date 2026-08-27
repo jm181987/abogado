bf26c57d6b514ad3718d7458517fcda954a7b670
Jorge Marquez <104573069+jm181987@users.noreply.github.com>
2026-08-27 11:18:48 -0300

    Load hidden-scrollbar carousel styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,6 +11,7 @@ import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 import carouselCleanupCss from "../carousel-cleanup.css?url";
+import carouselScrollbarCss from "../carousel-scrollbar.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { AuthProvider } from "@/hooks/use-auth";
 import { UiEnhancer } from "@/components/UiEnhancer";
@@ -67,6 +68,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     links: [
       { rel: "stylesheet", href: appCss },
       { rel: "stylesheet", href: carouselCleanupCss },
+      { rel: "stylesheet", href: carouselScrollbarCss },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
